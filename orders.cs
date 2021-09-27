98ca73f394af0f0d0b3552a5db5233325b383d8a
Harmeet Singh <harmeet@golfdiscount.com>
2021-09-26 23:05:35 -0700

    Added SSH tunneling

--- orders.cs
+++ orders.cs
@@ -1,12 +1,15 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Renci.SshNet;
+
 
 namespace magestack
 {
@@ -19,17 +22,56 @@ namespace magestack
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            Magestack server = new Magestack();
+            string result = server.ExecuteCmd("cd var/export/mmexportcsv && ls | grep PT_WSI_09_24");
+            server.CloseCxn();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            return new OkObjectResult("Current directory listing:\n" + result);
+        }
+    }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+    class Magestack
+    {
+        private readonly SshTunnel ssh;
 
-            return new OkObjectResult(responseMessage);
+        public Magestack()
+        {
+            ssh = new SshTunnel(Environment.GetEnvironmentVariable("stack_host"),
+                Environment.GetEnvironmentVariable("stack_user"),
+                Environment.GetEnvironmentVariable("stack_pass"));
+        }
+
+        public string ExecuteCmd(string cmd)
+        {
+            return ssh.ExecuteCommand(cmd);
+        }
+
+        public void CloseCxn()
+        {
+            ssh.Disconnect();
+        }
+    }
+
+    class SshTunnel
+    {
+        private readonly SshClient client;
+
+        public SshTunnel(string host, string user, string pass)
+        {
+            client = new SshClient(host, user, pass);
+            client.Connect();
+        }
+
+        public void Disconnect()
+        {
+            client.Disconnect();
+        }
+
+        public string ExecuteCommand(string cmd)
+        {
+            var command = client.CreateCommand(cmd);
+            command.Execute();
+            return command.Result;
         }
     }
 }
