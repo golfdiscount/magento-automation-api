ebd4110e7769d9f989c1ff6e78055803164cd0ef
Harmeet Singh <harmeet@golfdiscount.com>
2021-09-28 17:48:55 -0700

    Uploads files to WSI API for DB insertion and SFTP

--- Wsi.cs
+++ Wsi.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Magento;
 
@@ -13,28 +11,63 @@ namespace magestack
 {
     public class Wsi
     {
-        [FunctionName("listOrders")]
-        public void Run([TimerTrigger("45 8 * * 1-5")]TimerInfo timer, ILogger log)
+        [FunctionName("uploadOrders")]
+        public async Task<IActionResult> Run([TimerTrigger("45 8 * * 1-5")]TimerInfo timer, ILogger log)
         {
             string today = DateTime.Today.ToString("MM/dd/yyyy");
-            log.LogInformation($"Getting WSI order sheets for {today}");
-
+            log.LogInformation($"Looking for WSI order files for {today}...");
+            log.LogInformation("Connecting to SFTP server...");
             Magestack server = new Magestack();
             SftpClient sftp = server.CreateSftpClient();
+
             sftp.ChangeDir("var/export/mmexportcsv");
             List<Renci.SshNet.Sftp.SftpFile> files = sftp.List(
                 pattern: "PT_WSI_" + string.Format("{0:MM_dd_yyy}", DateTime.Today)
             );
 
-            log.LogInformation("Current directory listing:");
-            
-            foreach (Renci.SshNet.Sftp.SftpFile file in files)
+            log.LogInformation($"Found {files.Count} WSI files");
+            log.LogInformation($"Disconnecting from {server.Host} server");
+            server.Disconnect();
+            log.LogInformation("Processing files");
+
+            List<byte[]> fileByteArrays = ConvertFiles(files, sftp, log);
+            log.LogInformation("Uploading to WSI API");
+
+            HttpClient requester = new HttpClient();
+            // Uploading to WSI can take a while, timeout is set to 5 minutes
+            requester.Timeout = new TimeSpan(0, 5, 0);
+
+            foreach (byte[] file in fileByteArrays)
             {
-                log.LogInformation(file.Name);
+                await requester.PostAsync(Environment.GetEnvironmentVariable("wsi_url"), new ByteArrayContent(file));
             }
 
-            log.LogInformation("Disconnecting from server");
-            server.Disconnect();
+            return new OkObjectResult("All files processed and uploaded successfully");
+        }
+
+        private List<byte[]> ConvertFiles(List<Renci.SshNet.Sftp.SftpFile> files, SftpClient sftp, ILogger log)
+        {
+            string result = "";
+            List<byte[]> fileByteArrays = new List<byte[]>();
+            foreach (Renci.SshNet.Sftp.SftpFile file in files)
+            {
+                log.LogInformation($"Writing {file.Name}");
+                // Byte array of file contents
+                byte[] fileContents = sftp.ReadFile(file);
+
+                fileByteArrays.Add(fileContents);
+
+                // Concatenated value of byte array (a singular file)
+                string records = "";
+                foreach (byte value in fileContents)
+                {
+                    records += ((char)value).ToString();
+                }
+
+                result += records;
+            }
+
+            return fileByteArrays;
         }
     }
 }
