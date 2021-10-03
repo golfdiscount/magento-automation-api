228a4a35aad0b38749fb0ccd2e465aada1b86e71
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-03 15:21:19 -0700

    Added a route to get payment information for an order

---
+++ payments.cs
@@ -0,0 +1,46 @@
+﻿using Magento;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace magestack{
+
+	public class Payments
+	{
+		private readonly Magestack _server;
+
+		public Payments(Magestack server)
+		{
+			_server = server;
+		}
+
+		[FunctionName("payments")]
+		public async Task<JsonResult> Run(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "payment/{orderNum}")] HttpRequest req,
+			string orderNum
+            )
+        {
+			Dictionary<string, Dictionary<string, string>> results = new Dictionary<string, Dictionary<string, string>>();
+			string qry = "SELECT increment_id, entity_id, shipping_description " +
+				"FROM sales_order " +
+				$"WHERE increment_id=\"{orderNum}\"";
+
+			using (MySqlDataReader reader = await _server.ExecuteMySqlCommand(qry))
+            {
+				while (reader.Read())
+                {
+					Dictionary<string, string> values = new Dictionary<string, string>();
+					values.Add("entity_id", reader.GetString("entity_id"));
+					values.Add("shipping", reader.GetString("shipping_description"));
+					results.Add(reader.GetString("increment_id"), values);
+                }
+            }
+
+			return new JsonResult(results);
+        }
+	}
+}
