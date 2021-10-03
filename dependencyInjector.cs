797743984a063da198a6bc4eec8ac326935e9f27
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-03 02:28:54 -0700

    Initial commit

---
+++ dependencyInjector.cs
@@ -0,0 +1,35 @@
+﻿using Magento;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(magestack.Startup))]
+
+namespace magestack
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            Magestack server = new Magestack();
+            SshTunnel ssh = server.CreateSshClient();
+            SftpClient sftp = server.CreateSftpClient();
+
+            ssh.ForwardPort(
+                "127.0.0.1",
+                3307,
+                Environment.GetEnvironmentVariable("db_host"),
+                uint.Parse(Environment.GetEnvironmentVariable("db_port"))
+                );
+            server.CreateMySqlConn("127.0.0.1",
+                3307,
+                Environment.GetEnvironmentVariable("db_user"),
+                Environment.GetEnvironmentVariable("db_pass")
+                );
+
+            builder.Services.AddSingleton(server);
+            builder.Services.AddSingleton(ssh);
+            builder.Services.AddSingleton(sftp);
+        }
+    }
+}
