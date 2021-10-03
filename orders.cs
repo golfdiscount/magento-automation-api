1ed0dd479ea825224959c404743e98fd63c38f9f
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-03 01:25:44 -0700

    Renamed file to match route type

--- orders.cs
+++ orders.cs
@@ -15,34 +15,42 @@ namespace magestack
     {
         [FunctionName("orders")]
         public JsonResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "orders/{orderNum?}")] HttpRequest req,
+            string orderNum,
             ILogger log)
         {
             Magestack server = new Magestack();
             SshTunnel ssh = server.CreateSshClient();
+            
             // Create a tunnel with connection to DB bounded to 127.0.0.1:3307
             ssh.ForwardPort("127.0.0.1", 3307, Environment.GetEnvironmentVariable("db_host"), uint.Parse(Environment.GetEnvironmentVariable("db_port")));
             // Create a connection to the DB that was previously bounded to 127.0.0.1:3307
             server.CreateMySqlConn("127.0.0.1", 3307, Environment.GetEnvironmentVariable("db_user"), Environment.GetEnvironmentVariable("db_pass"));
-            MySqlDataReader reader = server.ExecuteMySqlCommand(
-                "SELECT increment_id, created_at, base_grand_total " +
-                "FROM golfdi_mage2.sales_order " +
-                "ORDER BY created_at " +
-                "DESC LIMIT 10; ");
-
+            
             Dictionary<string, Dictionary<string, string>> results = new Dictionary<string, Dictionary<string, string>>();
+            string qry = "SELECT increment_id, created_at, base_grand_total " +
+                "FROM golfdi_mage2.sales_order ";
 
-            while (reader.Read())
+            if (orderNum != null)
             {
-                Dictionary<string, string> values = new Dictionary<string, string>();
-                values.Add("created_at", reader.GetString("created_at"));
-                values.Add("base_grand_total", reader.GetString("base_grand_total"));
-                results.Add(reader.GetString("increment_id"), values);
+                qry += $"WHERE increment_id={orderNum} ";
             }
 
-            reader.Close();
-            server.Disconnect();
+            qry += "ORDER BY created_at " +
+                "DESC LIMIT 10;";
 
+            using (MySqlDataReader reader = server.ExecuteMySqlCommand(qry))
+            {
+                while (reader.Read())
+                {
+                    Dictionary<string, string> values = new Dictionary<string, string>();
+                    values.Add("created_at", reader.GetString("created_at"));
+                    values.Add("base_grand_total", reader.GetString("base_grand_total"));
+                    results.Add(reader.GetString("increment_id"), values);
+                }
+            }
+
+            server.Disconnect();
             return new JsonResult(results);
         }
     }
