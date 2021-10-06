0612273c0345c68e25fc9545a98b67087b0e1976
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-05 17:00:16 -0700

    Updated to work with new server implementation

--- Startup.cs
+++ Startup.cs
@@ -9,27 +9,13 @@ namespace magestack
 {
     public class Startup : FunctionsStartup
     {
-        public async override void Configure(IFunctionsHostBuilder builder)
+        public override void Configure(IFunctionsHostBuilder builder)
         {
             Magestack server = new Magestack();
-            SshTunnel ssh = server.CreateSshClient();
-            SftpClient sftp = server.CreateSftpClient();
 
-            ssh.ForwardPort(
-                "127.0.0.1",
-                3307,
-                Environment.GetEnvironmentVariable("db_host"),
-                uint.Parse(Environment.GetEnvironmentVariable("db_port"))
-                );
-            await server.CreateMySqlConn("127.0.0.1",
-                3307,
-                Environment.GetEnvironmentVariable("db_user"),
-                Environment.GetEnvironmentVariable("db_pass")
-                );
-
-            builder.Services.AddSingleton(server);
-            builder.Services.AddSingleton(ssh);
-            builder.Services.AddSingleton(sftp);
+            builder.Services.AddSingleton(server.Database);
+            builder.Services.AddSingleton(server.Sftp);
+            builder.Services.AddSingleton(server.Ssh);
         }
     }
 }
