7d8389abe96ea105efe4aad1441d0f709f7aafbf
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-27 09:15:00 -0700

    Added documentation

--- routes/payments.cs
+++ routes/payments.cs
@@ -8,15 +8,22 @@ using System.Collections.Generic;
 
 namespace magestack{
 
+	/// <summary> A Payments trigger </summary>
 	public class Payments
 	{
 		private readonly MagentoDb _db;
 
+		/// <summary> Initializes a Payments object</summary>
+		/// <param name="db"> The connection to the Magento database</param>
 		public Payments(MagentoDb db)
 		{
 			_db = db;
 		}
 
+		/// <summary> Triggers a run of the payments route </summary>
+		/// <param name="req"> HttpRequest to this endpoint </param>
+		/// <param name="orderNum"> Order number passed in as a route parameter </param>
+		/// <returns></returns>
 		[FunctionName("payments")]
 		public JsonResult Run(
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "payments/{orderNum}")] HttpRequest req,
