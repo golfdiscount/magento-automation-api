755b8c074f1d36c0c775ec6866870501683db084
Harmeet Singh <harmeet@golfdiscount.com>
2022-03-03 08:52:21 -0800

    Added UPC to product query

--- routes/products.cs
+++ routes/products.cs
@@ -40,8 +40,11 @@ namespace magestack.routes
 
             log.LogInformation($"Searching for {sku} in the database");
 
-            string qry = $@"SELECT v1.value AS 'name', e.sku,
-            FORMAT(d1.value, 2) AS 'price', t1.value AS 'short_description'
+            string qry = $@"SELECT v1.value AS 'name',
+                e.sku,
+                FORMAT(d1.value, 2) AS 'price',
+                t1.value AS 'short_description',
+                v2.value AS 'upc'
             FROM catalog_product_entity e
             LEFT JOIN catalog_product_entity_varchar v1 ON e.entity_id = v1.entity_id
             AND v1.store_id = 0
@@ -73,6 +76,16 @@ namespace magestack.routes
                     SELECT entity_type_id
                     FROM eav_entity_type
                     WHERE entity_type_code = 'catalog_product'))
+            LEFT JOIN catalog_product_entity_varchar v2 ON e.entity_id = v2.entity_id
+            AND v2.store_id = 0
+            AND v2.attribute_id =(
+	            SELECT attribute_id
+	            FROM eav_attribute
+	            WHERE attribute_code = 'upc'
+		            AND entity_type_id = (
+			            SELECT entity_type_id
+			            FROM eav_entity_type
+			            WHERE entity_type_code = 'catalog_product'))
             WHERE e.sku = '{sku}';";
 
             MySqlCommand dbCmd = new MySqlCommand(qry, _cnx);
@@ -92,6 +105,7 @@ namespace magestack.routes
                 result.Add("sku", dataReader.GetString(1));
                 result.Add("price", dataReader.GetString(2));
                 result.Add("description", dataReader.GetString(3));
+                result.Add("upc", dataReader.GetString(4));
             }
 
             dataReader.Close();
