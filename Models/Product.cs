37927f4b92ea44e837d5ac290b8993ae6ace5297
Harmeet Singh <harmeet@golfdiscount.com>
2022-07-08 12:52:59 -0700

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace magestack.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public string sku { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public string upc { get; set; }
+        public int quantity { get; set; }
+    }
+}
