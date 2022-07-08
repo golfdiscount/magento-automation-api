185a08768bb7736d92abdd9646e2b8e98e5dea8b
Harmeet Singh <harmeet@golfdiscount.com>
2022-07-08 12:53:09 -0700

    Initial commit

---
+++ Queue Triggers/CacheProduct.cs
@@ -0,0 +1,33 @@
+using magestack.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
+
+namespace magestack.Queue_Triggers
+{
+    public class CacheProduct
+    {
+        private readonly IDistributedCache cache;
+
+        public CacheProduct(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+
+        [FunctionName("CacheProduct")]
+        public void Run([QueueTrigger("cache-products")]string queueItem, ILogger log)
+        {
+            Product product = JsonSerializer.Deserialize<Product>(queueItem);
+
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = new TimeSpan(24, 0, 0)
+            };
+
+            log.LogInformation($"Caching {product.sku} in Redis cache");
+            cache.SetString(product.sku, queueItem, options);
+        }
+    }
+}
