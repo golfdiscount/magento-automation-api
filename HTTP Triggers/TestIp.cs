36f6ea9a181b4d9a5d8423f353a9dbaaccbd3047
Harmeet Singh <harmeet@golfdiscount.com>
2022-07-13 00:30:43 -0700

    Fixed type

--- HTTP Triggers/TestIp.cs
+++ HTTP Triggers/TestIp.cs
@@ -18,7 +18,7 @@ namespace magestack.HTTP_Triggers
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             var client = new HttpClient();
-            var response = await client.GetAsync("https://ifocnfig.me");
+            var response = await client.GetAsync("https://ifconfig.me");
             var responseMessage = await response.Content.ReadAsStringAsync();
 
             log.LogInformation(responseMessage);
