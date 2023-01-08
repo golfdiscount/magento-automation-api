6584f43a8e47561f1f5f2ec4d94091779255b5cd
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 19:54:30 -0800

    Initial commit

---
+++ Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace magestack.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Upc { get; set; }
+    }
+}
