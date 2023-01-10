d410ca1c0f602a9d19baec80783350b5d1cfbacc
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 16:54:44 -0800

    Initial commit

---
+++ Data/Orders.cs
@@ -0,0 +1,101 @@
+﻿using magestack.Models;
+using MySql.Data.MySqlClient;
+
+namespace magestack.Data
+{
+    public static class Orders
+    {
+        private enum AddressType
+        {
+            Shipping,
+            Billing
+        }
+
+        public static OrderModel GetOrder (string incrementId, MySqlConnection conn)
+        {
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT sales_order.entity_id, " +
+                "increment_id, " +
+                "state, " +
+                "status, " +
+                "shipping_description, " +
+                "FORMAT(sales_order.base_grand_total, 2) AS 'total', " +
+                "FORMAT(sales_order.base_shipping_amount, 2) AS 'shipping_total', " +
+                "created_at, " +
+                "updated_at, " +
+                "payment.method " +
+                "FROM sales_order " +
+                "JOIN sales_order_payment AS payment ON payment.parent_id = sales_order.entity_id " +
+                "WHERE increment_id = @increment_id;";
+            cmd.Parameters.AddWithValue("@increment_id", incrementId);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            
+            if (!reader.HasRows)
+            {
+                return null;
+            }
+
+            reader.Read();
+
+            OrderModel order = new()
+            {
+                Id = reader.GetInt32("entity_id"),
+                OrderNumber = reader.GetString("increment_id"),
+                State = reader.GetString("state"),
+                Status = reader.GetString("status"),
+                Shipping = reader.GetString("shipping_description"),
+                PaymentMethod = reader.GetString("method"),
+                OrderTotal = reader.GetDecimal("total"),
+                ShippingTotal = reader.GetDecimal("shipping_total"),
+                CreatedAt = reader.GetDateTime("created_at"),
+                UpdatedAt = reader.GetDateTime("updated_at")
+            };
+
+            reader.Close();
+
+            order.Customer = GetAddress(order.Id, AddressType.Billing, conn);
+            order.Recipient = GetAddress(order.Id, AddressType.Shipping, conn);
+            order.Products = Products.GetProductByOrder(order.Id, conn);
+
+            return order;
+        }
+        private static AddressModel GetAddress(int entityId, AddressType addressType, MySqlConnection conn)
+        {
+            using MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT CONCAT(firstname, ' ', lastname) AS 'name'," +
+                "street," +
+                "city," +
+                "region AS 'state'," +
+                "country_id AS 'country'," +
+                "postcode AS 'zip' " +
+                "FROM sales_order_address " +
+                "WHERE parent_id = @entity_id " +
+                "AND address_type = @address_type;";
+            cmd.Parameters.AddWithValue("@entity_id", entityId);
+
+            if (addressType == AddressType.Shipping)
+            {
+                cmd.Parameters.AddWithValue("@address_type", "shipping");
+            } else
+            {
+                cmd.Parameters.AddWithValue("@address_type", "billing");
+            }           
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            AddressModel address = new()
+            {
+                Name = reader["name"].ToString(),
+                Street = reader["street"].ToString(),
+                City = reader["city"].ToString(),
+                State = reader["state"].ToString(),
+                Country = reader["country"].ToString(),
+                Zip = reader["zip"].ToString()
+            };
+
+            return address;
+        }
+    }
+}
---
+++ Data/Products.cs
@@ -0,0 +1,114 @@
+﻿using magestack.Models;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace magestack.Data
+{
+    public static class Products
+    {
+        public static ProductModel GetProduct(string sku, MySqlConnection conn)
+        {
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $@"SELECT v1.value AS 'name',
+                    e.sku,
+                    FORMAT(d1.value, 2) AS 'price',
+                    t1.value AS 'short_description',
+                    v2.value AS 'upc'
+                FROM catalog_product_entity e
+                LEFT JOIN catalog_product_entity_varchar v1 ON e.entity_id = v1.entity_id
+                AND v1.store_id = 0
+                AND v1.attribute_id =(
+                    SELECT attribute_id
+                    FROM eav_attribute
+                    WHERE attribute_code = 'name'
+                    AND entity_type_id = (
+                        SELECT entity_type_id
+                        FROM eav_entity_type
+                        WHERE entity_type_code = 'catalog_product'))
+                LEFT JOIN catalog_product_entity_text t1 ON e.entity_id = t1.entity_id
+                AND t1.store_id = 0
+                AND t1.attribute_id = (
+                    SELECT attribute_id
+                    FROM eav_attribute
+                    WHERE attribute_code = 'short_description'
+                    AND entity_type_id = (
+                        SELECT entity_type_id
+                        FROM eav_entity_type
+                        WHERE entity_type_code = 'catalog_product'))
+                LEFT JOIN catalog_product_entity_decimal d1 ON e.entity_id = d1.entity_id
+                AND d1.store_id = 0
+                AND d1.attribute_id = (
+                    SELECT attribute_id
+                    FROM eav_attribute
+                    WHERE attribute_code = 'price'
+                    AND entity_type_id = (
+                        SELECT entity_type_id
+                        FROM eav_entity_type
+                        WHERE entity_type_code = 'catalog_product'))
+                LEFT JOIN catalog_product_entity_varchar v2 ON e.entity_id = v2.entity_id
+                AND v2.store_id = 0
+                AND v2.attribute_id =(
+	                SELECT attribute_id
+	                FROM eav_attribute
+	                WHERE attribute_code = 'upc'
+		                AND entity_type_id = (
+			                SELECT entity_type_id
+			                FROM eav_entity_type
+			                WHERE entity_type_code = 'catalog_product'))
+                WHERE e.sku = @sku;";
+            cmd.Parameters.AddWithValue("@sku", sku);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (!reader.HasRows)
+            {
+                return null;
+            }
+
+            reader.Read();
+
+            ProductModel product = new()
+            {
+                Name = reader.IsDBNull(0) ? null : reader.GetString("name"),
+                Sku = reader.IsDBNull(1) ? null : reader.GetString("sku"),
+                Price = reader.IsDBNull(2) ? 0 : reader.GetDecimal("price"),
+                Description = reader.IsDBNull(3) ? null : reader.GetString("short_description"),
+                Upc = reader.IsDBNull(4) ? null : reader.GetString("upc")
+            };
+
+            return product;
+        }
+
+        public static List<ProductModel> GetProductByOrder(int entity_id, MySqlConnection conn)
+        {
+            using MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT DISTINCT sku FROM sales_order_item WHERE order_id = @entity_id;";
+            cmd.Parameters.AddWithValue("@entity_id", entity_id);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (!reader.HasRows)
+            {
+                return null;
+            }
+
+            List<string> skus = new();
+
+            while (reader.Read())
+            {
+                skus.Add(reader.GetString("sku"));
+            }
+
+            reader.Close();
+
+            List<ProductModel> products = new();
+
+            foreach (string sku in skus)
+            {
+                products.Add(GetProduct(sku, conn));
+            }
+
+            return products;
+        }
+    }
+}
