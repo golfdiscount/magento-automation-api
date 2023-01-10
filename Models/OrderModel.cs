73ace00d211cf6884329de8175801ded52d75a53
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 16:54:53 -0800

    Initial commit

---
+++ Models/OrderModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace magestack.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+
+        public string OrderNumber { get; set; }
+
+        public string State { get; set; }
+
+        public string Status { get; set; }
+
+        public string Shipping { get; set; }
+
+        public AddressModel Customer { get; set; }
+
+        public AddressModel Recipient { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public decimal OrderTotal { get; set; }
+
+        public decimal ShippingTotal { get; set; }
+
+        public List<ProductModel> Products { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
