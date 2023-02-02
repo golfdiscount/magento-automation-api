524725156435df8362d4eb04ad21c8d877103ad2
BuildTools <unconfigured@null.spigotmc.org>
2023-02-01 20:45:25 -0800

    Added parsing of CSV files and uploading by single order

--- TimerTriggers/UploadWSIOrders.cs
+++ TimerTriggers/UploadWSIOrders.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Pgd.Magento.Models.Wsi;
 using Renci.SshNet;
 using Renci.SshNet.Sftp;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -40,6 +42,8 @@ public class UploadWsiOrders
             if (sftp.WorkingDirectory != EXPORT_PATH) sftp.ChangeDirectory("var/export/mmexportcsv");
 
             IEnumerable<SftpFile> files = sftp.ListDirectory(sftp.WorkingDirectory);
+
+            // Files containing WSI orders and should be archived after successful upload
             List<SftpFile> wsiFiles = new();
 
             // These are files that indicate there were no orders to generate at that time stamp
@@ -54,32 +58,33 @@ public class UploadWsiOrders
                 if (ptRgx.IsMatch(file.Name) && !file.IsDirectory)
                 {
                     wsiFiles.Add(file);
+                    string csvContents = sftp.ReadAllText(file.FullName).Trim();
+                    IEnumerable<PickTicketModel> pickTickets = ParseCsv(csvContents);
+
+                    foreach (PickTicketModel pickTicket in pickTickets)
+                    {
+                        log.LogInformation($"Submitting {pickTicket.PickTicketNumber}");
+
+                        HttpRequestMessage request = new(HttpMethod.Post, "api/picktickets")
+                        {
+                            Content = JsonContent.Create(pickTicket)
+                        };
+
+                        HttpResponseMessage response = await wsiClient.SendAsync(request);
+
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            log.LogError($"Unable to create pick ticket {pickTicket.PickTicketNumber}: {response.ReasonPhrase}");
+                        } else
+                        {
+                            log.LogInformation($"Created {pickTicket.PickTicketNumber}");
+                        }
+                    }
+
                 } else if (noUpdateRgx.IsMatch(file.Name) && !file.IsDirectory)
                 {
                     noUpdateFiles.Add(file);
                 }
-            }
-
-            if (wsiFiles.Count != 0)
-            {
-                log.LogInformation($"Found {wsiFiles.Count} WSI file(s)");
-                log.LogInformation("Joining files");
-                List<byte> fileBytes = new();
-
-                foreach (SftpFile file in wsiFiles)
-                {
-                    fileBytes.AddRange(sftp.ReadAllBytes(file.FullName));
-                }
-
-                HttpRequestMessage requestMessage = new(HttpMethod.Post, "api/picktickets")
-                {
-                    Content = new ByteArrayContent(fileBytes.ToArray())
-                };
-                requestMessage.Content.Headers.ContentType = new("text/csv");
-                HttpResponseMessage response = await wsiClient.SendAsync(requestMessage);
-                response.EnsureSuccessStatusCode();
-
-                log.LogInformation("File sent to WSI successfully");
             }
 
             // Move PT_WSI files to the PT_archive directory
@@ -109,5 +114,70 @@ public class UploadWsiOrders
         {
             sftp.Disconnect();
         }
+    }
+
+    /// <summary>
+    /// Parses a CSV containg pickticket headers and details
+    /// </summary>
+    /// <param name="csv">CSV to parse</param>
+    /// <returns>A key value of pair of order number to an OrderModel</returns>
+    private static IEnumerable<PickTicketModel> ParseCsv(string csv)
+    {
+        Dictionary<string, PickTicketModel> orders = new();
+        string[] records = csv.Split("\n");
+
+        foreach (string record in records)
+        {
+            string[] fields = record.Split(',');
+            string recordType = fields[0];
+            string pickticketNum = fields[2];
+
+            if (!orders.ContainsKey(pickticketNum))
+            {
+                orders.Add(pickticketNum, new());
+                orders[pickticketNum].PickTicketNumber = pickticketNum;
+                orders[pickticketNum].LineItems = new();
+                orders[pickticketNum].Channel = 1;
+            }
+
+            PickTicketModel order = orders[pickticketNum];
+
+            if (recordType == "PTH")
+            {
+                order.OrderNumber = fields[3];
+                order.OrderDate = DateTime.ParseExact(fields[5], "MM/dd/yyyy", null);
+                order.Customer = new()
+                {
+                    Name = fields[12].Replace("\"", ""),
+                    Street = fields[13].Replace("\"", ""),
+                    City = fields[14].Replace("\"", ""),
+                    State = fields[15],
+                    Country = fields[16],
+                    Zip = fields[17]
+                };
+                order.Recipient = new()
+                {
+                    Name = fields[19].Replace("\"", ""),
+                    Street = fields[20].Replace("\"", ""),
+                    City = fields[21].Replace("\"", ""),
+                    State = fields[22],
+                    Country = fields[23],
+                    Zip = fields[24]
+                };
+                order.ShippingMethod = fields[32];
+                order.Store = 1;
+            }
+            else if (recordType == "PTD")
+            {
+                order.LineItems.Add(new()
+                {
+                    Sku = fields[5],
+                    Units = int.Parse(fields[10]),
+                    LineNumber = int.Parse(fields[3])
+                });
+            }
+        }
+
+        return orders.Values;
     }
 }
